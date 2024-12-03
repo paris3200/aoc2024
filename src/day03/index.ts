a559059796d7c0a20e90c6e2cbbf033dcca963ab
Jason Paris <paris3200@gmail.com>
2024-12-03 10:47:21 -0500

    wip

--- src/day03/index.ts
+++ src/day03/index.ts
@@ -4,6 +4,15 @@ const parseInput = (rawInput: string) => {
   return rawInput.split("\n");
 };
 
+const parseFunction = (input: string) => {
+  let strip1 = input.replace("mul(", "");
+  let strip2 = strip1.replace(")", "");
+  const [input1, input2] = strip2.split(",");
+  const num1 = parseInt(input1);
+  const num2 = parseInt(input2);
+  return num1 * num2;
+};
+
 const parseValidInput = (input: string): number => {
   const re = /mul\(\d+,\s*\d+\)/g;
   let products = [];
@@ -44,27 +53,66 @@ const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
   const re = /mul\(\d+,\s*\d+\)/g;
+  const doRe = /do\(\)/g;
+  const donotRe = /don't\(\)/g;
+  let sum = 0;
 
-  const onResults = [];
   for (let x = 0; x < input.length; x++) {
-    const result = input[x].split("don't");
+    let match;
+    let products = [];
+    let dos = [{ index: 0 }];
+    let donts = [];
+    while ((match = re.exec(input[x]))) {
+      let product = parseFunction(match[0]);
+      products.push({ product: product, index: match.index });
+    }
 
-    for (let y = 0; y < result.length; y += 2) {
-      onResults.push(result[y]);
+    let doMatch;
+    while ((doMatch = doRe.exec(input[x]))) {
+      dos.push({ index: doMatch.index });
+    }
+    let donotMatch;
+    while ((donotMatch = donotRe.exec(input[x]))) {
+      donts.push({ index: donotMatch.index });
+    }
+
+    donts.reverse();
+    dos.reverse();
+
+    console.log(products);
+    console.log("Don'ts:", donts);
+    console.log("Dos:", dos);
+
+    let off = donts.pop()?.index;
+    let on = dos.pop()?.index;
+
+    for (let x = 0; x < products.length; x++) {
+      console.log("\nProduct: ", products[x]);
+      console.log("On: ", on);
+      console.log("Off: ", off);
+      if (
+        products[x].index > on &&
+        (products[x].index < off || off === undefined)
+      ) {
+        console.log(
+          `Valid: index (${products[x].index}) is between ${on} and ${off} `,
+        );
+        // sum.push(products[x].product);
+        sum += products[x].product;
+        products.slice(x, 1);
+      } else {
+        console.log(
+          `Invalid: index (${products[x].index}) is not between ${on} and ${off} `,
+        );
+        if (products[x].index > off) {
+          off = donts.pop()?.index;
+          on = dos.pop()?.index;
+        }
+      }
     }
   }
 
-  let products = [];
-
-  for (let x = 0; x < onResults.length; x++) {
-    products.push(parseValidInput(onResults[x]));
-  }
-
-  return products
-    .reduce((sum, value) => {
-      return sum + value;
-    }, 0)
-    .toString();
+  return sum.toString();
 };
 
 run({
@@ -87,5 +135,5 @@ run({
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 });
