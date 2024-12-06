9032e7c3cb807b9589f71c872e3542581f42aacd
Jason Paris <paris3200@gmail.com>
2024-12-06 13:02:21 -0500

    solve: D6P1

--- README.md
+++ README.md
@@ -16,7 +16,7 @@
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%86/yellow)](src/day05)
-![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%86/yellow)](src/day06)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
@@ -97,16 +97,16 @@ Both parts: 3.827ms
 
 ```
 Day 05
-Time part 1: 3.722ms
+Time part 1: 3.975ms
 Time part 2: -
-Both parts: 3.722ms
+Both parts: 3.975ms
 ```
 
 ```
 Day 06
-Time part 1: -
+Time part 1: 21.738ms
 Time part 2: -
-Both parts: -
+Both parts: 21.738ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 9/50
-Total time: 18.473ms
+Total stars: 10/50
+Total time: 40.464ms
 ```
 
 <!--/RESULTS-->
--- src/day06/index.ts
+++ src/day06/index.ts
@@ -1,33 +1,139 @@
 import run from "aocrunner";
+import { dir } from "console";
+import { start } from "repl";
 
+let GRID: [[string | boolean]] = [];
 const parseInput = (rawInput: string) => {
   const lines = rawInput.split("\n");
-  let grid = [];
+  let origin = [0, 0];
   for (let i = 0; i < lines.length; i++) {
-    grid[i] = [];
+    GRID[i] = [];
     for (let j = 0; j < lines[i].length; j++) {
-      grid[i].push(lines[i][j]);
+      if (lines[i][j] === "^") {
+        origin = [i, j];
+      }
+      GRID[i].push(lines[i][j]);
     }
   }
-  return grid;
+  return origin;
 };
 
-const printGrid = (grid) => {
-  for (let i = 0; i < grid.length; i++) {
+const printGrid = () => {
+  for (let i = 0; i < GRID.length; i++) {
     let line = "";
-    for (let j = 0; j < grid[i].length; j++) {
-      line += grid[i][j];
+    for (let j = 0; j < GRID[i].length; j++) {
+      if (GRID[i][j] === true) {
+        line += "X";
+      } else {
+        line += GRID[i][j];
+      }
     }
     console.log(line);
   }
 };
 
+const countVisited = () => {
+  let count = 0;
+  for (let i = 0; i < GRID.length; i++) {
+    let line = "";
+    for (let j = 0; j < GRID[i].length; j++) {
+      if (GRID[i][j] === true) {
+        count += 1;
+      }
+    }
+  }
+  return count;
+};
+
+const setVisited = (i: number, j: number) => {
+  if (GRID[i][j] !== true) {
+    GRID[i][j] = true;
+  }
+};
+
+const move = (direction: string, starting: [number, number]) => {
+  if (direction === "N") {
+    console.log("Moving North");
+    console.log(starting);
+    for (let i = starting[0]; i >= 0; i--) {
+      if (GRID[i][starting[1]] === "#") {
+        console.log("Hit Wall");
+        return [i + 1, starting[1]];
+      } else if (i - 1 < 0) {
+        setVisited(i, starting[1]);
+        return -1;
+      }
+      setVisited(i, starting[1]);
+    }
+  }
+  if (direction === "E") {
+    console.log("Moving East");
+    console.log("Starting East: ", starting);
+    for (let j = starting[1]; j < GRID[0].length + 1; j++) {
+      if (GRID[starting[0]][j] === "#") {
+        return [starting[0], j - 1];
+      } else if (j + 1 == GRID[0].length) {
+        setVisited(starting[0], j);
+        return -1;
+      }
+      setVisited(starting[0], j);
+    }
+  }
+  if (direction === "S") {
+    console.log("Moving South");
+    console.log(starting);
+    for (let i = starting[0]; i < GRID.length; i++) {
+      if (GRID[i][starting[1]] === "#") {
+        return [i - 1, starting[1]];
+      } else if (i + 1 > GRID.length - 1) {
+        setVisited(i, starting[1]);
+        return -1;
+      }
+      setVisited(i, starting[1]);
+    }
+  }
+  if (direction === "W") {
+    console.log("Moving West");
+    console.log("Starting West: ", starting);
+    for (let j = starting[1]; j >= 0; j--) {
+      if (GRID[starting[0]][j] === "#") {
+        return [starting[0], j + 1];
+      } else if (j - 1 < 0) {
+        setVisited(starting[0], j);
+        return -1;
+      }
+      setVisited(starting[0], j);
+    }
+  }
+};
+
 const part1 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+  let point = parseInput(rawInput);
+  console.log("Origin:", point);
+  let directions = ["N", "E", "S", "W"];
+  let directionCount = 0;
+  printGrid();
 
-  let direction = "N";
+  const updateDirectionCount = () => {
+    if (directionCount === directions.length - 1) {
+      directionCount = 0;
+    } else {
+      directionCount += 1;
+    }
+  };
 
-  return;
+  while (point !== -1 && point !== undefined) {
+    point = move(directions[directionCount], point);
+    if (point !== -1 && point !== undefined) {
+      console.log("Point:", point);
+      updateDirectionCount();
+      console.log("Stopped at:", point);
+    } else if (point === -1) {
+      console.log("Solved");
+    }
+  }
+  printGrid();
+  return countVisited().toString();
 };
 
 const part2 = (rawInput: string) => {
@@ -65,5 +171,5 @@ run({
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 });
