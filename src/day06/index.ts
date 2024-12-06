fb08ccd0f3d53321866c029ddd384bda517841b9
Jason Paris <paris3200@gmail.com>
2024-12-06 18:44:46 -0500

    wip: D6P2

--- src/day06/index.ts
+++ src/day06/index.ts
@@ -1,6 +1,9 @@
 import run from "aocrunner";
 
+const DEBUG = true;
 let GRID: [[string | boolean]] = [];
+let visitedPoints = new Map();
+
 const parseInput = (rawInput: string): number[] => {
   const lines = rawInput.split("\n");
   let origin = [0, 0];
@@ -71,9 +74,15 @@ const move = (
           return [i + 1, starting[1]];
         } else if (i - 1 < 0) {
           setVisited(i, starting[1]);
+          if (updateVisited([i, starting[1]], "N") == false) {
+            return 1;
+          }
           return -1;
         }
         setVisited(i, starting[1]);
+        if (updateVisited([i, starting[1]], "N") == false) {
+          return 1;
+        }
       }
       break;
 
@@ -83,9 +92,15 @@ const move = (
           return [starting[0], j - 1];
         } else if (j + 1 == GRID[0].length) {
           setVisited(starting[0], j);
+          if (updateVisited([starting[0], j], "E") == false) {
+            return 1;
+          }
           return -1;
         }
         setVisited(starting[0], j);
+        if (updateVisited([starting[0], j], "E") == false) {
+          return 1;
+        }
       }
       break;
 
@@ -95,9 +110,15 @@ const move = (
           return [i - 1, starting[1]];
         } else if (i + 1 > GRID.length - 1) {
           setVisited(i, starting[1]);
+          if (updateVisited([i, starting[1]], "S") == false) {
+            return 1;
+          }
           return -1;
         }
         setVisited(i, starting[1]);
+        if (updateVisited([i, starting[1]], "S") == false) {
+          return 1;
+        }
       }
       break;
 
@@ -110,6 +131,9 @@ const move = (
           return -1;
         }
         setVisited(starting[0], j);
+        if (updateVisited([starting[0], j], "W") == false) {
+          return 1;
+        }
       }
       break;
 
@@ -117,6 +141,17 @@ const move = (
       throw new Error(`Invalid direction: ${direction}`);
   }
   return -1;
+};
+
+const updateVisited = (point: number[], direction: string) => {
+  let pointLabel = point[0] + "-" + point[1] + direction;
+  if (visitedPoints.has(pointLabel)) {
+    console.log("Loop Detected at point:", point);
+    GRID[point[0]][point[1]] = "!";
+    return false;
+  } else {
+    visitedPoints.set(pointLabel, true);
+  }
 };
 
 const solveGrid = (start: number[]): boolean => {
@@ -133,15 +168,7 @@ const solveGrid = (start: number[]): boolean => {
     }
   };
 
-  const visitedPoints = new Map();
-
   while (Array.isArray(point)) {
-    let pointLabel = point[0] + "-" + point[1] + directions[directionCount];
-    if (visitedPoints.has(pointLabel)) {
-      return false;
-    } else {
-      visitedPoints.set(pointLabel, true);
-    }
     point = move(directions[directionCount], point);
     updateDirectionCount();
   }
@@ -151,6 +178,9 @@ const solveGrid = (start: number[]): boolean => {
     return true;
   }
 
+  if (point === 1) {
+    return false;
+  }
   return false;
 };
 
@@ -163,39 +193,47 @@ const part1 = (rawInput: string) => {
 
 const part2 = (rawInput: string) => {
   const origin = parseInput(rawInput);
-  const gridCopyUnsolved = structuredClone(GRID);
-  solveGrid(origin);
-  let gridCopySolved = structuredClone(GRID);
 
-  let visitedPoints = getVisited();
+  // Copy Original Grid
+  const gridCopyUnsolved = structuredClone(GRID);
+
+  solveGrid(origin);
+
+  let visitedPointArray = getVisited();
   let loopCount = 0;
 
+  // Which points resulted in a loop.
   let addedPoints = [];
-  visitedPoints.forEach((points) => {
+
+  visitedPointArray.forEach((points: [number, number]) => {
+    visitedPoints.clear();
+    console.log("Visited Points Map:", visitedPoints);
     GRID = structuredClone(gridCopyUnsolved);
     GRID[points[0]][points[1]] = "#";
     let isSolved = solveGrid(origin);
     if (isSolved === false) {
+      if (DEBUG) {
+        GRID[origin[0]][origin[1]] = "^";
+        GRID[points[0]][points[1]] = "0";
+        printGrid();
+      }
       addedPoints.push([points[0], points[1]]);
       loopCount += 1;
     }
   });
-
-  // console.log(addedPoints);
-  // addedPoints.forEach((point) => {
-  //   console.log("Point:", point);
-  //   GRID = structuredClone(gridCopyUnsolved);
-  //   GRID[point[0]][point[1]] = "0";
-  //   printGrid();
-  //   console.log();
-  // });
-
-  // GRID = gridCopyUnsolved;
-  // GRID[6][3] = "#";
-  // let isLoop = solveGrid(origin);
-  // printGrid();
-  // console.log(isLoop);
-
+  // const points = [7, 6];
+  // GRID = structuredClone(gridCopyUnsolved);
+  // GRID[points[0]][points[1]] = "#";
+  // let isSolved = solveGrid(origin);
+  // if (isSolved === false) {
+  //   //Obstacle Added at this location
+  //   GRID[origin[0]][origin[1]] = "^";
+  //   GRID[points[0]][points[1]] = "0";
+  //
+  //   console.log(printGrid());
+  //   addedPoints.push([points[0], points[1]]);
+  //   loopCount += 1;
+  // }
   return loopCount.toString();
 };
 
@@ -237,5 +275,5 @@ run({
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: false,
+  onlyTests: true,
 });
