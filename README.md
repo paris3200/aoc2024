878b61555e7e2f228bb13840d7a3430d0a94935c
Jason Paris <paris3200@gmail.com>
2024-12-08 18:31:56 -0500

    solve: D8P2

--- README.md
+++ README.md
@@ -18,7 +18,7 @@
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%86/yellow)](src/day05)
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%86/yellow)](src/day06)
 [![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
-![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/11/%E2%98%86%E2%98%86/gray)
@@ -97,9 +97,9 @@ Both parts: 3.719ms
 
 ```
 Day 05
-Time part 1: 3.975ms
+Time part 1: 4.068ms
 Time part 2: -
-Both parts: 3.975ms
+Both parts: 4.068ms
 ```
 
 ```
@@ -111,16 +111,16 @@ Both parts: 2.659ms
 
 ```
 Day 07
-Time part 1: 4467.094ms
-Time part 2: 215410.368ms
-Both parts: 219877.462ms
+Time part 1: 4414.007ms
+Time part 2: 211544.896ms
+Both parts: 215958.903ms
 ```
 
 ```
 Day 08
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 1.919ms
+Time part 2: 5.361ms
+Both parts: 7.28ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 12/50
-Total time: 219898.739ms
+Total stars: 14/50
+Total time: 215987.553ms
 ```
 
 <!--/RESULTS-->
--- src/day08/index.ts
+++ src/day08/index.ts
@@ -1,6 +1,6 @@
 import run from "aocrunner";
 
-const debug = false;
+let debug = false;
 const log = (...str: any[]) => {
   if (debug) {
     console.log(...str);
@@ -71,13 +71,92 @@ const calcVectorPoints = (pt1: [number, number], pt2: [number, number]) => {
   const pt2X = pt2[1];
   const pt2Y = pt2[0];
 
-  const xHalf = (pt1X - pt2X) * 2;
-  const yHalf = (pt1Y - pt2Y) * 2;
+  const [deltaY, deltaX] = getSlope(pt1, pt2);
 
-  const pointBeyond1 = [pt1Y + yHalf, pt1X + xHalf];
-  const pointBeyond2 = [pt2Y - yHalf, pt2X - xHalf];
+  const pointBeyond1 = [pt1Y + deltaY, pt1X + deltaX];
+  const pointBeyond2 = [pt2Y - deltaY, pt2X - deltaX];
 
   return [pointBeyond1, pointBeyond2];
+};
+
+const getSlope = (pt1: [number, number], pt2: [number, number]) => {
+  const pt1X = pt1[1];
+  const pt1Y = pt1[0];
+  const pt2X = pt2[1];
+  const pt2Y = pt2[0];
+
+  const deltaX = pt1X - pt2X;
+  const deltaY = pt1Y - pt2Y;
+
+  return [deltaY, deltaX];
+};
+
+const isInt = (value: string | number) => {
+  return Number.isInteger(Number(value));
+};
+
+const calcLine = (
+  pt1: [number, number],
+  pt2: [number, number],
+  maxX: number,
+  maxY: number,
+) => {
+  const points = [pt1, pt2];
+  const [deltaY, deltaX] = getSlope(pt1, pt2);
+  const antinodes = new Set();
+
+  points.forEach((point) => {
+    let currentX = point[1] + deltaX;
+    let currentY = point[0] + deltaY;
+
+    //Add
+    while (
+      currentX <= maxX &&
+      currentX >= 0 &&
+      currentY <= maxY &&
+      currentY >= 0
+    ) {
+      currentX += deltaX;
+      currentY += deltaY;
+
+      if (
+        currentX <= maxX &&
+        currentX >= 0 &&
+        currentY <= maxY &&
+        currentY >= 0
+      ) {
+        if (isInt(currentX) && isInt(currentY)) {
+          antinodes.add([currentY, currentX].toString());
+        }
+      }
+    }
+
+    currentX = point[1] - deltaX;
+    currentY = point[0] - deltaY;
+    //Subtract
+    while (
+      currentX <= maxX &&
+      currentX >= 0 &&
+      currentY <= maxY &&
+      currentY >= 0
+    ) {
+      currentX -= deltaX;
+      currentY -= deltaY;
+
+      if (
+        currentX <= maxX &&
+        currentX >= 0 &&
+        currentY <= maxY &&
+        currentY >= 0
+      ) {
+        if (isInt(currentX) && isInt(currentY)) {
+          antinodes.add([currentY, currentX].toString());
+        }
+      }
+    }
+  });
+
+  return antinodes;
 };
 
 const part1 = (rawInput: string) => {
@@ -88,7 +167,7 @@ const part1 = (rawInput: string) => {
   const antinode = new Set();
 
   const antennas = getAntennas(input);
-  console.log(antennas);
+  log(antennas);
 
   for (const [frequency, points] of antennas) {
     log("Frequency", frequency);
@@ -96,40 +175,62 @@ const part1 = (rawInput: string) => {
     let lines = getPermutations(2, points);
 
     for (const line of lines) {
-      const [node1, node2] = calcVectorPoints(line[0], line[1]);
-
-      if (
-        node1[0] < maxY &&
-        node1[0] >= 0 &&
-        node1[1] < maxX &&
-        node1[1] >= 0
-      ) {
-        antinode.add(node1);
-      }
-
-      if (
-        node2[0] < maxY &&
-        node2[0] >= 0 &&
-        node2[1] < maxX &&
-        node2[1] >= 0
-      ) {
-        antinode.add(node2);
+      const nodes = calcVectorPoints(line[0], line[1]);
+      for (const node of nodes) {
+        if (node[0] < maxY && node[0] >= 0 && node[1] < maxX && node[1] >= 0) {
+          antinode.add(node.toString());
+        }
       }
     }
-
-    printGrid(input);
-    console.log();
   }
 
-  console.log(antinode);
   return antinode.size.toString();
 };
 
 const part2 = (rawInput: string) => {
+  debug = false;
   const input = parseInput(rawInput);
-  log(input);
+  const maxX = input[0].length - 1;
+  const maxY = input.length - 1;
 
-  return;
+  log("Max X:", maxX);
+  log("Max Y:", maxY);
+  const antinode = new Set();
+  const antennas = getAntennas(input);
+  log(antennas);
+
+  for (const [frequency, points] of antennas) {
+    log("Frequency", frequency);
+    log("Points:", points);
+    let lines = getPermutations(2, points);
+
+    for (const line of lines) {
+      antinode.add(line[0].toString());
+      antinode.add(line[1].toString());
+      let harmonics = calcLine(line[0], line[1], maxX, maxY);
+      for (const harmonic of harmonics) {
+        antinode.add(harmonic);
+      }
+
+      const nodes = calcVectorPoints(line[0], line[1]);
+      for (const node of nodes) {
+        if (node[0] < maxY && node[0] >= 0 && node[1] < maxX && node[1] >= 0) {
+          antinode.add(node.toString());
+        }
+      }
+    }
+  }
+
+  if (debug) {
+    console.log(antinode);
+    const points = Array.from(antinode, (pair) => pair.split(",").map(Number));
+    console.log(points);
+    for (const point of points) {
+      input[point[0]][point[1]] = "#";
+    }
+    printGrid(input);
+  }
+  return antinode.size.toString();
 };
 
 run({
@@ -168,13 +269,22 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `T.........
+...T......
+.T........
+..........
+..........
+..........
+..........
+..........
+..........
+..........`,
+        expected: "9",
+      },
     ],
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 });
