5a5a4ada45f7bd626aa6a5b3fc02ccb324a428f4
Jason Paris <paris3200@gmail.com>
2024-12-08 16:03:28 -0500

    wip: D8

--- src/day08/index.ts
+++ src/day08/index.ts
@@ -1,8 +1,8 @@
 import run from "aocrunner";
 
-const onlyTests = true;
+const debug = false;
 const log = (...str: any[]) => {
-  if (onlyTests) {
+  if (debug) {
     console.log(...str);
   }
 };
@@ -43,13 +43,86 @@ const getAntennas = (
   return result;
 };
 
+const getPermutations = (items: number, options: string[]): string[][] => {
+  const result: string[][] = [];
+
+  function backtrack(permutation: string[], remainingOptions: string[]): void {
+    if (permutation.length === items) {
+      result.push([...permutation]);
+      return;
+    }
+
+    for (let i = 0; i < remainingOptions.length; i++) {
+      const option = remainingOptions[i];
+      backtrack(
+        [...permutation, option],
+        [...remainingOptions.slice(0, i), ...remainingOptions.slice(i + 1)],
+      );
+    }
+  }
+
+  backtrack([], options);
+  return result;
+};
+
+const calcVectorPoints = (pt1: [number, number], pt2: [number, number]) => {
+  const pt1X = pt1[1];
+  const pt1Y = pt1[0];
+  const pt2X = pt2[1];
+  const pt2Y = pt2[0];
+
+  const xHalf = (pt1X - pt2X) * 2;
+  const yHalf = (pt1Y - pt2Y) * 2;
+
+  const pointBeyond1 = [pt1Y + yHalf, pt1X + xHalf];
+  const pointBeyond2 = [pt2Y - yHalf, pt2X - xHalf];
+
+  return [pointBeyond1, pointBeyond2];
+};
+
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
-  printGrid(input);
+  const maxX = input[0].length;
+  const maxY = input.length;
 
-  log(getAntennas(input));
+  const antinode = new Set();
 
-  return;
+  const antennas = getAntennas(input);
+  console.log(antennas);
+
+  for (const [frequency, points] of antennas) {
+    log("Frequency", frequency);
+    log("Points:", points);
+    let lines = getPermutations(2, points);
+
+    for (const line of lines) {
+      const [node1, node2] = calcVectorPoints(line[0], line[1]);
+
+      if (
+        node1[0] < maxY &&
+        node1[0] >= 0 &&
+        node1[1] < maxX &&
+        node1[1] >= 0
+      ) {
+        antinode.add(node1);
+      }
+
+      if (
+        node2[0] < maxY &&
+        node2[0] >= 0 &&
+        node2[1] < maxX &&
+        node2[1] >= 0
+      ) {
+        antinode.add(node2);
+      }
+    }
+
+    printGrid(input);
+    console.log();
+  }
+
+  console.log(antinode);
+  return antinode.size.toString();
 };
 
 const part2 = (rawInput: string) => {
@@ -75,6 +148,21 @@ run({
 ..........`,
         expected: "2",
       },
+      {
+        input: `............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............`,
+        expected: "14",
+      },
     ],
     solution: part1,
   },
@@ -88,5 +176,5 @@ run({
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests,
+  onlyTests: true,
 });
